0acd699daf381cc04199a51cf7a67157733045f8
bautd-2046 <tran.duy.bau@sun-asterisk.com>
2021-03-23 14:21:35 +0700

    refs task [FE] Create Post type program

--- app/public/wp-content/mu-plugins/university_post_types.php
+++ app/public/wp-content/mu-plugins/university_post_types.php
@@ -1,21 +1,38 @@
 <?php
-function university_post_types()
-{
-    register_post_type('event', array(
-        'rewrite' => array('slug' => 'events'),
-        'supports' => array('title', 'editor', 'excerpt'),
-        'show_in_rest' => true,
-        'has_archive' => true,
-        'public' => true,
-        'labels' => array(
-            'name' => 'Events',
-            'add_new_item' => 'Add New Event',
-            'edit_item' => 'Edit Event',
-            'all_items' => 'All Events',
-            'singular_name' => 'Event'
-        ),
-        'menu_icon' => 'dashicons-calendar'
-    ));
+function university_post_types() {
+	// Event post type
+	register_post_type( 'event', array(
+		'rewrite'      => array( 'slug' => 'events' ),
+		'supports'     => array( 'title', 'editor', 'excerpt' ),
+		'show_in_rest' => true,
+		'has_archive'  => true,
+		'public'       => true,
+		'labels'       => array(
+			'name'          => 'Events',
+			'add_new_item'  => 'Add New Event',
+			'edit_item'     => 'Edit Event',
+			'all_items'     => 'All Events',
+			'singular_name' => 'Event'
+		),
+		'menu_icon'    => 'dashicons-calendar'
+	) );
+
+	// Program post type
+	register_post_type( 'program', array(
+		'rewrite'      => array( 'slug' => 'programs' ),
+		'supports'     => array( 'title', 'editor' ),
+		'show_in_rest' => true,
+		'has_archive'  => true,
+		'public'       => true,
+		'labels'       => array(
+			'name'          => 'Programs',
+			'add_new_item'  => 'Add New Program',
+			'edit_item'     => 'Edit Program',
+			'all_items'     => 'All Programs',
+			'singular_name' => 'Program'
+		),
+		'menu_icon'    => 'dashicons-awards'
+	) );
 }
 
-add_action('init', 'university_post_types');
+add_action( 'init', 'university_post_types' );
--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -1,33 +1,31 @@
 <?php
 
-function university_files()
-{
+function university_files() {
 //    wp_enqueue_script('main-university-javascript', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
-    wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('goole_font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+	wp_enqueue_style( 'font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+	wp_enqueue_style( 'goole_font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i' );
 //    wp_enqueue_style('university_main_style', get_stylesheet_uri());
-    if (strstr($_SERVER['SERVER_NAME'], 'wpsml.local')) {
-        wp_enqueue_script('main-university-javascript', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
-    } else {
-        wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), NULL, '1.0', true);
-        wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.bc49dbb23afb98cfc0f7.js'), NULL, '1.0', true);
-        wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.bc49dbb23afb98cfc0f7.css'));
-    }
+	if ( strstr( $_SERVER['SERVER_NAME'], 'wpsml.local' ) ) {
+		wp_enqueue_script( 'main-university-javascript', 'http://localhost:3000/bundled.js', null, '1.0', true );
+	} else {
+		wp_enqueue_script( 'our-vendors-js', get_theme_file_uri( '/bundled-assets/vendors~scripts.8c97d901916ad616a264.js' ), null, '1.0', true );
+		wp_enqueue_script( 'main-university-js', get_theme_file_uri( '/bundled-assets/scripts.bc49dbb23afb98cfc0f7.js' ), null, '1.0', true );
+		wp_enqueue_style( 'our-main-styles', get_theme_file_uri( '/bundled-assets/styles.bc49dbb23afb98cfc0f7.css' ) );
+	}
 }
 
-add_action('wp_enqueue_scripts', 'university_files');
+add_action( 'wp_enqueue_scripts', 'university_files' );
 // wp_enqueue_scripts -> lúc khởi tạo nạp file, chạy hàm university_files
 
-function university_features()
-{
+function university_features() {
 //    register_nav_menu('headerMenuLocation', 'Header Menu Location'); // đăng ký 1 cái menu (kiểu như nav-header-menu)
 //    register_nav_menu('footerExploreMenuLocation', 'Footer Explore Menu Location');
 //    register_nav_menu('footerLearnMenuLocation', 'Footer Learn Menu Location');
 
-    add_theme_support('title-tag');
+	add_theme_support( 'title-tag' );
 }
 
-add_action('after_setup_theme', 'university_features');
+add_action( 'after_setup_theme', 'university_features' );
 
 //function university_post_types()
 //{
@@ -42,22 +40,33 @@ add_action('after_setup_theme', 'university_features');
 //
 //add_action('init', 'university_post_types');
 
-function university_adjust_queries($query) {
-	if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
-		$today = date('Ymd');
+function university_adjust_queries( $query ) {
+	if ( ! is_admin() and is_main_query() ) {
+		// event post type
+		if ( is_post_type_archive( 'event' ) ) {
+			$today = date( 'Ymd' );
 
-		$query->set('meta_key', 'event_date');
-		$query->set('orderby', 'meta_value_num');
-		$query->set('order', 'ASC');
-		$query->set('meta_query', array(
-			array(
-				'key' => 'event_date',
-				'compare' => '>=',
-				'value' => $today,
-				'type' => 'numeric',
-			),
-		));
+			$query->set( 'meta_key', 'event_date' );
+			$query->set( 'orderby', 'meta_value_num' );
+			$query->set( 'order', 'ASC' );
+			$query->set( 'meta_query', array(
+				array(
+					'key'     => 'event_date',
+					'compare' => '>=',
+					'value'   => $today,
+					'type'    => 'numeric',
+				),
+			) );
+		}
+
+		// Program post type
+		if ( is_post_type_archive( 'program' ) ) {
+			$query->set( 'orderby', 'title' );
+			$query->set( 'order', 'ASC' );
+			$query->set( 'posts_per_page', - 1 );
+		}
 	}
+
 }
 
-add_action('pre_get_posts', 'university_adjust_queries');
+add_action( 'pre_get_posts', 'university_adjust_queries' );
