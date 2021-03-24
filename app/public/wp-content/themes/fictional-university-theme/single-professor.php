37d300340d74d0c6a1d18a176f70de44298de4e3
bautd-2046 <tran.duy.bau@sun-asterisk.com>
2021-03-24 16:32:05 +0700

    refs task [FE] Professor post_type and Image resizes

---
+++ app/public/wp-content/themes/fictional-university-theme/single-professor.php
@@ -0,0 +1,63 @@
+<?php
+get_header();
+
+while ( have_posts() ) {
+	the_post();
+	?>
+    <div class="page-banner">
+        <div class="page-banner__bg-image"
+             style="background-image: url(<?php echo get_field( 'page_banner_background_image' )['sizes']['pageBanner']; ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title() ?></h1>
+            <div class="page-banner__intro">
+                <p><?php the_field( 'page_banner_subtitle' ); ?></p>
+            </div>
+        </div>
+    </div>
+    <div class="container container--narrow page-section">
+        <div class="generic-content">
+            <div class="row group">
+                <div class="one-third">
+					<?php
+					the_post_thumbnail( 'professorPortrait' );
+					?>
+                </div>
+                <div class="two-third">
+					<?php
+					the_content();
+					?>
+                </div>
+            </div>
+        </div>
+		<?php
+		$theRelatedPrograms = get_field( 'related_programs' );
+
+		if ( $theRelatedPrograms ) {
+			echo '<hr class="section-break">';
+			echo '<h2 class="headline headline--medium">Subject(s) Taught</h2>';
+			echo '<ul class="link-list min-list">';
+			foreach ( $theRelatedPrograms as $program ) {
+				?>
+                <li>
+                    <a href="<?php echo get_the_permalink( $program ); ?>">
+						<?php echo get_the_title( $program ); ?>
+                    </a>
+                </li>
+				<?php
+			}
+			echo '</ul>';
+		}
+		?>
+    </div>
+	<?php
+}
+
+get_footer();
+?>
+
+<!--<h2>--><?php //the_title(); ?><!--</h2>-->
+<!--<section>-->
+<!--    --><?php //the_content(); ?>
+<!--</section>-->
+<!--<a href="/">Back to home</a>-->
+<!--why this still works?-->
