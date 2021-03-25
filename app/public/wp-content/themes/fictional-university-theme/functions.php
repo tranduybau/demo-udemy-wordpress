a733e40480fbd32949aa931db6949ecb580323e9
bautd-2046 <tran.duy.bau@sun-asterisk.com>
2021-03-25 19:37:35 +0700

    refs task [FE] Create modal and JS logical for modal search

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -35,9 +35,9 @@ function university_files() {
 	if ( strstr( $_SERVER['SERVER_NAME'], 'wpsml.local' ) ) {
 		wp_enqueue_script( 'main-university-javascript', 'http://localhost:3000/bundled.js', null, '1.0', true );
 	} else {
-		wp_enqueue_script( 'our-vendors-js', get_theme_file_uri( '/bundled-assets/vendors~scripts.8c97d901916ad616a264.js' ), null, '1.0', true );
-		wp_enqueue_script( 'main-university-js', get_theme_file_uri( '/bundled-assets/scripts.bc49dbb23afb98cfc0f7.js' ), null, '1.0', true );
-		wp_enqueue_style( 'our-main-styles', get_theme_file_uri( '/bundled-assets/styles.bc49dbb23afb98cfc0f7.css' ) );
+		wp_enqueue_script( 'our-vendors-js', get_theme_file_uri( '/bundled-assets/vendors~scripts.1fa169383e64a33bfd0c.js' ), null, '1.0', true );
+		wp_enqueue_script( 'main-university-js', get_theme_file_uri( '/bundled-assets/scripts.4dd930fda1e207b35b8f.js' ), null, '1.0', true );
+		wp_enqueue_style( 'our-main-styles', get_theme_file_uri( '/bundled-assets/styles.4dd930fda1e207b35b8f.css' ) );
 	}
 }
 
